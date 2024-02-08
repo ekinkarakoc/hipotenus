ccaf2ae2382d4320d572906cbadb2a8152e229fe
ekin <ekinkarakoc1@gmail.com>
2024-02-08 05:31:15 +0300

    "second commit"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        int a,b;
+
+        double c;
+
+        System.out.println("birinci kenarı giriniz");
+
+        a = scanner.nextInt();
+
+        System.out.println("ikinci kenarı giriniz");
+
+        b = scanner.nextInt();
+
+        c = Math.sqrt(a*a + b*b);
+
+        System.out.println("hipotenus : "+ c);
     }
 }
